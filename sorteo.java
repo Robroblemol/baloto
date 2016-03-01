71e98e1bacee66f8fb6b7cbe8967307e85bd279c
Roberto Robles <robertodavidrobles@gmail.com>
2016-03-01 13:42:55 -0500

    Correcion: no genera numeros repetidos

--- sorteo.java
+++ sorteo.java
@@ -1,14 +1,20 @@
 import java.lang.Math;
 public class sorteo {
   public int [] start (int [] array){
+    boolean frepetido = false;
     int i = 0, alet = 0;
     while(i < array.length){
       alet = (int) (Math.random()*45+0);//para generar numero entero aletorio de 45 a 0
       //System.out.println("numero generado "+alet);
-      if(alet <= 45 && alet >= 0){
-        array[i]=alet;
-        i++;
-      }
+        for(int j = 0; j <= i;j++){
+          if(array[j] == alet)
+          frepetido=true;
+          }
+        if(frepetido == false){
+          array[i]=alet;
+          i++;
+          }
+        else frepetido = false;
     }
     return array;
   }
